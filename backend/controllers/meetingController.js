3421d74789e856e03b70a892fe9ddf80b5d52e96
phaserunner03 <bhavya.shah@searce.com>
2025-02-27 19:59:16 +0530

    create meeting

--- backend/controllers/meetingController.js
+++ backend/controllers/meetingController.js
@@ -1,27 +1,30 @@
 const meetingService = require('../services/meetingService');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
 
 const scheduleMeeting = async (req, res) => {
-  try {
-    const { title, description, participants, startTime, endTime } = req.body;
-    const organizerId=req.user?.id;
-    if (!title || !startTime || !endTime) {
-      return res.status(400).json({ message: 'Missing required fields' });
-    }
+//   try {
+//     const { accessToken, eventData } = req.body; // Get token from frontend
+//     if (!accessToken) {
+//         return res.status(401).json({ message: 'Unauthorized: No access token provided' });
+//     }
 
-    const result = await meetingService.scheduleMeeting({
-      organizerId,
-      title,
-      description,
-      participants,
-      startTime,
-      endTime,
-    });
-
-    res.status(201).json(result);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+//     const event = await createEvent(accessToken, eventData);
+//     res.status(200).json(event);
+// } catch (error) {
+//     res.status(500).json({ message: error.message });
+// }
+try{
+  const {user} = req;
+  const {title,location,description,participants,startTime,endTime} = req.body;
+  const event = await meetingService.scheduleMeeting(user, title, location,description, participants, startTime, endTime);
+  res.status(200).json(event);
+}
+catch(err){
+  res.status(500).json({message:err.message});
+}
+}
 
 const getAllMeetings = async (req, res) => {
   try {
@@ -31,5 +34,17 @@ const getAllMeetings = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+const test = async (req,res)=>{
+  try{
+    const token = req.header('authToken');
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    const user = await User.findOne({googleId:decoded.uid});
+    console.log(user)
+    res.status(200).json({message:"Token verified"});
+  }
+  catch(err){
 
-module.exports = { scheduleMeeting, getAllMeetings };
+  }
+}
+
+module.exports = { scheduleMeeting, getAllMeetings,test};

5e51fddc30fc73bf6165e32bbe2a756107415785
phaserunner03 <bhavya.shah@searce.com>
2025-03-24 12:54:14 +0530

    removed comments

--- backend/src/config/gcpConfig.ts
+++ backend/src/config/gcpConfig.ts
@@ -2,7 +2,7 @@ import { Storage } from "@google-cloud/storage";
 import path from "path";
 
 const storage = new Storage({
-  keyFilename: path.join(__dirname, process.env.GCP_SERVICE_KEY_PATH ?? ""), // Path to downloaded JSON
+  keyFilename: path.join(__dirname, process.env.GCP_SERVICE_KEY_PATH ?? ""), 
   projectId: process.env.GCP_PROJECT_ID,
 });
 

0c366cc9e9c0a4442051e47a2180352f6d39888d
phaserunner03 <bhavya.shah@searce.com>
2025-03-25 16:19:19 +0530

    gcsConfig changed

--- backend/src/config/gcpConfig.ts
+++ backend/src/config/gcpConfig.ts
@@ -1,10 +1,7 @@
 import { Storage } from "@google-cloud/storage";
 import path from "path";
 
-const storage = new Storage({
-  keyFilename: path.join(__dirname, process.env.GCP_SERVICE_KEY_PATH ?? ""), 
-  projectId: process.env.GCP_PROJECT_ID,
-});
+const storage = new Storage();
 
 const bucketName = process.env.GCP_BUCKET_NAME ?? "cloudcapture-bucket";
 

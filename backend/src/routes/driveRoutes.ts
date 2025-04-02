4e664ec62d5359fb2264059fad5865c737a3db16
phaserunner03 <bhavya.shah@searce.com>
2025-04-02 15:55:04 +0530

    bug fixed

--- backend/src/routes/driveRoutes.ts
+++ backend/src/routes/driveRoutes.ts
@@ -1,16 +1,16 @@
 import express from "express";
 import * as driveController from "../controllers/driveController";
 import { authMiddleware } from "../middleware/authMiddleware";
-import { AuthenticatedRequest } from "../controllers/authController";
+import { AuthenticatedRequest } from "../constants/types.constants";
 
 //restrictTo check
 
 const router = express.Router();
-router.get("/folders", authMiddleware, async (req, res, next) => driveController.getAllFolders(req as AuthenticatedRequest, res).catch(next));
+router.get("/folders", authMiddleware, async (req, res, next) => driveController.getAllFolders(req as unknown as AuthenticatedRequest, res).catch(next));
 
-router.get("/folders/:folderId", authMiddleware, async (req, res, next) => driveController.getFilesInFolder(req as AuthenticatedRequest, res).catch(next));
+router.get("/folders/:folderId", authMiddleware, async (req, res, next) => driveController.getFilesInFolder(req as unknown as  AuthenticatedRequest, res).catch(next));
 
-router.delete("/files/:fileId", authMiddleware, async (req, res, next) => driveController.deleteFile(req as AuthenticatedRequest, res).catch(next));
+router.delete("/files/:fileId", authMiddleware, async (req, res, next) => driveController.deleteFile(req as unknown as AuthenticatedRequest, res).catch(next));
 export default router;
 
 

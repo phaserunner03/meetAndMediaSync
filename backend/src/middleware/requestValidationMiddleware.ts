e581b673c0a9d1d2eaaed92007ed8402e9f88fe4
RidamChhapiya <ridam.chhapiya@searce.com>
2025-04-03 12:29:37 +0530

    added some logger

--- backend/src/middleware/requestValidationMiddleware.ts
+++ backend/src/middleware/requestValidationMiddleware.ts
@@ -11,13 +11,27 @@
 // diagram  
 import { Request, Response, NextFunction } from "express";
 import * as yup from "yup";
+import logger from "../utils/logger";
 
 export const validateRequest = (schema: yup.ObjectSchema<any>) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       await schema.validate(req.body, { abortEarly: false });
+      logger.info({
+        functionName: "validateRequest",
+        message: "Request body validated successfully",
+        data: req.body,
+      });
       next();
     } catch (err) {
+        logger.error({
+        functionName: "validateRequest",
+        message: "Request body validation failed",
+        data: {
+          name: (err as Error).name,
+          stack: (err as Error).stack,
+        },
+        });
       if (err instanceof yup.ValidationError) {
         return res.status(400).json({
           errors: err.errors,

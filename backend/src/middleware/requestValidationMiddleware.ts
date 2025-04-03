fe1b5a193f5b8abf76f356d001d7700ec98f753a
RidamChhapiya <ridam.chhapiya@searce.com>
2025-04-03 15:05:50 +0530

    changes

--- backend/src/middleware/requestValidationMiddleware.ts
+++ backend/src/middleware/requestValidationMiddleware.ts
@@ -1,14 +1,3 @@
-//winston logging
-//yup validation 
-//edge cases validation service level
-// read me file 
-
-//frontend -> deployment -> 1) prod user  2) dev -> changes append (CICD)
-//backend -> deployment -> 1) prod user  2) dev -> changes append (CICD)
-//staging 
-
-
-// diagram  
 import { Request, Response, NextFunction } from "express";
 import * as yup from "yup";
 import logger from "../utils/logger";

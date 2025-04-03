9865f40110bfa7db6a50c0a262c4618cf200d8d2
phaserunner03 <bhavya.shah@searce.com>
2025-04-03 11:51:11 +0530

    middleware initiated for ref

--- backend/src/middleware/requestValidationMiddleware.ts
+++ backend/src/middleware/requestValidationMiddleware.ts
@@ -1,11 +1,29 @@
-//winston logging
-//yup validation 
-//edge cases validation service level
-// read me file 
+import {Request, Response, NextFunction} from 'express';
+import * as yup from "yup";
+import { StatusCodes } from '../constants/status-codes.constants';
+import { ErrorResponseMessages } from '../constants/service-messages.constants';
 
-//frontend -> deployment -> 1) prod user  2) dev -> changes append (CICD)
-//backend -> deployment -> 1) prod user  2) dev -> changes append (CICD)
-//staging 
+export const validateRequest = (schema: yup.ObjectSchema<any>) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        
+        await schema.validate(req.body, { abortEarly: false });
+        next(); 
+      } catch (error) {
+        if (error instanceof yup.ValidationError) {
+          
+          return res.status(StatusCodes.BAD_REQUEST).json({
+            success: false,
+            message: ErrorResponseMessages.VALIDATION_ERROR,
+            errors: error.errors,
+          });
+        }
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+          success: false,
+          message: ErrorResponseMessages.INTERNAL_ERROR,
+          error: (error as Error).message,
+        });
+      }
+    };
+  };
 
-
-// diagram  
--- backend/src/routes/meetingRoutes.ts
+++ backend/src/routes/meetingRoutes.ts
@@ -2,6 +2,10 @@ import express from "express";
 import * as meetingController from "../controllers/meetingController";
 import { authMiddleware, restrictTo } from "../middleware/authMiddleware";
 import { Permissions } from "../constants/permissions.constants";
+import { validateRequest } from "../middleware/requestValidationMiddleware";
+import {scheduleMeetingRequest,updateMeetingRequest, verifyMeetingRequest,} from "../schemas/meetings.schema";
+
+
 const router = express.Router();
 
 export interface AuthenticatedRequest extends express.Request {
@@ -11,9 +15,9 @@ export interface AuthenticatedRequest extends express.Request {
 }
 
 router.get("/all", authMiddleware, restrictTo(Permissions.VIEW_USER), (req, res, next) => meetingController.getAllMeetings(req as AuthenticatedRequest, res).catch(next));
-router.post("/schedule", authMiddleware, restrictTo(Permissions.CREATE_MEETING), (req, res, next) => meetingController.scheduleMeeting(req as   AuthenticatedRequest, res).catch(next));
-router.put("/update/:eventId", authMiddleware, restrictTo(Permissions.EDIT_MEETING),(req, res, next) =>  meetingController.updateMeeting(req as AuthenticatedRequest, res).catch(next));
+router.post("/schedule", authMiddleware, restrictTo(Permissions.CREATE_MEETING),validateRequest(scheduleMeetingRequest), (req, res, next) => meetingController.scheduleMeeting(req as   AuthenticatedRequest, res).catch(next));
+router.put("/update/:eventId", authMiddleware, restrictTo(Permissions.EDIT_MEETING),validateRequest(updateMeetingRequest),(req, res, next) =>  meetingController.updateMeeting(req as AuthenticatedRequest, res).catch(next));
 router.delete("/delete/:eventId", authMiddleware, restrictTo(Permissions.DELETE_MEETING), (req, res, next) => meetingController.deleteMeeting(req as AuthenticatedRequest, res).catch(next));
-router.post("/verify",(req, res) => meetingController.verifyMeeting(req as AuthenticatedRequest, res));
+router.post("/verify",validateRequest(verifyMeetingRequest),(req, res) => meetingController.verifyMeeting(req as AuthenticatedRequest, res));
 
 export default router;

b6b81988c03f98d01a2e4b7d9d4008e3e1a5d174
RidamChhapiya <ridam.chhapiya@searce.com>
2025-04-08 02:04:25 +0530

    added swagger documentation

--- backend/src/routes/reportRoutes.ts
+++ backend/src/routes/reportRoutes.ts
@@ -5,7 +5,24 @@ import { AuthenticatedRequest } from "../constants/types.constants";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Reports
+ *   description: Report generation APIs
+ */
 
-router.get("/report", authMiddleware, async (req, res, next) => generateReport(req as unknown as  AuthenticatedRequest, res).catch(next));
+/**
+ * @swagger
+ * /reports/v1/report:
+ *   get:
+ *     summary: Generate a report
+ *     tags: [Reports]
+ *     responses:
+ *       200:
+ *         description: Report generated successfully
+ */
+
+router.get("/report", authMiddleware, async (req, res, next) => generateReport(req as unknown as AuthenticatedRequest, res).catch(next));
 
 export default router;

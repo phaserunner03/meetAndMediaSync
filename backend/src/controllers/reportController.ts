2ef87d58c1303b351b1e1fe8f372f3206f2a00c3
RidamChhapiya <ridam.chhapiya@searce.com>
2025-04-07 15:53:27 +0530

    changes

--- backend/src/controllers/reportController.ts
+++ backend/src/controllers/reportController.ts
@@ -1,14 +1,32 @@
 import { Response } from "express";
 import { fetchMeetings } from "../services/reportService";
 import { AuthenticatedRequest } from "../constants/types.constants";
+import { ErrorResponseMessages } from "../constants/service-messages.constants";
+import { StatusCodes } from "../constants/status-codes.constants";
+import logger from "../utils/logger";
 
 
 export const generateReport = async (req: AuthenticatedRequest, res: Response) => {
+  const functionName = "generateReport";
+  logger.info({ functionName, message: "Function started" });
   try {
     const reportData = await fetchMeetings(req.query);
-    res.status(200).json(reportData);
+    logger.info({ 
+        functionName, 
+        statusCode: StatusCodes.OK, 
+        message: "Report data fetched successfully", 
+        data: reportData });
+    res.status(StatusCodes.OK).json(reportData);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
+    logger.error({ 
+        functionName, 
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR, 
+        message: "Error occurred", 
+        error });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: ErrorResponseMessages.INTERNAL_ERROR });
+  } finally {
+    logger.info({ 
+        functionName, 
+        message: "Function ended" });
   }
 };

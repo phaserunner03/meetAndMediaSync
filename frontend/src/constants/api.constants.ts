7f854ad60d732fb3c56192473740ea732f798a75
bhavya418 <shahb3703@gmail.com>
2025-04-24 12:31:34 +0530

    url change

--- frontend/src/constants/api.constants.ts
+++ frontend/src/constants/api.constants.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL = "https://backend-972397341408.us-central1.run.app/web";
-export const API_URL = "https://cloudcapture-972397341408.us-central1.run.app";
+export const API_URL = "https://meetandmediasync-972397341408.us-central1.run.app"
 // export const API_BASE_URL = import.meta.env.VITE_API_URL ;
 // export const API_URL =  import.meta.env.URL ;
 
